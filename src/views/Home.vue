65d4dba7375553a2c72b31e7ef869927104dffb8
skl <sun.keliang@foxmail.com>
2022-01-28 04:18:14 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+.home test
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  components: {},
+})
+export default class Home extends Vue {}
+</script>
