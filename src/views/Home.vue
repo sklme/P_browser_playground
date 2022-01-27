88267648c48d70707f6f8c7a59546c03fe957068
skl <sun.keliang@foxmail.com>
2022-01-28 04:22:16 +0800

    fetch

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,5 +8,13 @@ import { Options, Vue } from 'vue-class-component';
 @Options({
   components: {},
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  created(): void {
+    fetch('www.example.com').then((response) => {
+      console.log(response);
+      console.log(response.headers);
+      return response.body;
+    });
+  }
+}
 </script>
